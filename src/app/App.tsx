104aff2e868c3c2eebf3d1f1a02fe2d3ed55acb8
Mykola Kutsil <mkutsil.dev@gmail.com>
2025-08-31 17:48:44 +0300

    feature: added Mantine UI library

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,13 @@
 import './App.scss';
 import './styles/reset.scss';
+import '@mantine/core/styles.css';
 import { AppRouter } from './providers/router';
+import { ThemeProvider } from './providers/ThemeProvider';
 
 const App = () => (
-    <>
+    <ThemeProvider>
         <AppRouter />
-    </>
+    </ThemeProvider>
 );
 
 export default App;
--- src/pages/HomePage/ui/HomePage.tsx
+++ src/pages/HomePage/ui/HomePage.tsx
@@ -1,15 +1,32 @@
 import { RoutePath } from '../../../shared/config/routeConfig/routeConfig';
 import Logo from '../../../shared/assets/logo.png';
+import { useMantineColorScheme, ActionIcon } from '@mantine/core';
+import { Sun, Moon } from 'lucide-react';
 
-const HomePage = () => (
-    <>
-        <h1>TaskNest</h1>
-        <a href={RoutePath.tasks}>
-            <img src={Logo} className="logo" alt="TaskNest logo" />
-        </a>
+const HomePage = () => {
+    const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+    const dark = colorScheme === 'dark';
 
-        <p>TaskNest — Nest your tasks, boost your productivity.</p>
-    </>
-);
+    return (
+        <>
+            <h1>TaskNest</h1>
+            <a href={RoutePath.tasks}>
+                <img src={Logo} className="logo" alt="TaskNest logo" />
+            </a>
+
+            <p>TaskNest — Nest your tasks, boost your productivity.</p>
+
+            <ActionIcon
+                onClick={() => toggleColorScheme()}
+                title="Toggle color scheme"
+                color="teal"
+                variant="light"
+                size={42}
+            >
+                {dark ? <Sun size={24} /> : <Moon size={24} />}
+            </ActionIcon>
+        </>
+    );
+};
 
 export default HomePage;
